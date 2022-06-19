7427f23d673b4643dae6dbc5d58dfaa8a0b9d076
류경석 <narzis@gmail.com>
2022-06-19 16:05:09 +0900

    JWT Service

---
+++ JWTExample/Migrations/20220619065220_init.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JWTExample.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeID", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "FirstName1", "LastName1" },
+                    { 2, "FirstName2", "LastName2" },
+                    { 3, "FirstName3", "LastName3" },
+                    { 4, "FirstName4", "LastName4" },
+                    { 5, "FirstName5", "LastName5" },
+                    { 6, "FirstName6", "LastName6" },
+                    { 7, "FirstName7", "LastName7" },
+                    { 8, "FirstName8", "LastName8" },
+                    { 9, "FirstName9", "LastName9" },
+                    { 10, "FirstName10", "LastName10" },
+                    { 11, "FirstName11", "LastName11" },
+                    { 12, "FirstName12", "LastName12" },
+                    { 13, "FirstName13", "LastName13" },
+                    { 14, "FirstName14", "LastName14" },
+                    { 15, "FirstName15", "LastName15" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
--- JWTExample/Program.cs
+++ JWTExample/Program.cs
@@ -1,11 +1,72 @@
+using JWTExample.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var securityScheme = new OpenApiSecurityScheme()
+{
+    Name = "Authorization",
+    Type = SecuritySchemeType.ApiKey,
+    Scheme = "Bearer",
+    BearerFormat = "JWT",
+    In = ParameterLocation.Header,
+    Description = "JSON Web Token based security",
+};
+
+var securityReq = new OpenApiSecurityRequirement()
+                {
+                    {
+                     new OpenApiSecurityScheme
+                     {
+                     Reference = new OpenApiReference
+                     {
+                     Type = ReferenceType.SecurityScheme,
+                     Id = "Bearer"
+                     }
+                     },
+                     new string[] {}
+                    }
+                };
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.AddSecurityDefinition("Bearer", securityScheme);
+    o.AddSecurityRequirement(securityReq);
+});
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddAuthentication(o =>
+{
+    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
 
 var app = builder.Build();
 
@@ -16,8 +77,56 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//JWT 
+app.MapGet("/api/employees",
+[Authorize] (AppDbContext db) =>
+{
+    return Results.Ok(db.Employees.ToList());
+});
+
+
+
+app.MapPost("/api/employees",
+[Authorize] (AppDbContext db, Employee emp) =>
+{
+    db.Employees.Add(emp);
+    db.SaveChanges();
+    return Results.Created
+($"/api/employees/{emp.EmployeeID}", emp);
+});
+
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapPost("/api/security/getToken",
+[AllowAnonymous] (User user) =>
+{
+
+    if (user.UserName == "user1" && user.Password == "password1")
+    {
+        var issuer = builder.Configuration["Jwt:Issuer"];
+        var audience = builder.Configuration["Jwt:Audience"];
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(issuer: issuer,
+            audience: audience,
+            signingCredentials: credentials);
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var stringToken = tokenHandler.WriteToken(token);
+
+        return Results.Ok(stringToken);
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+
+
 
 app.Run();
